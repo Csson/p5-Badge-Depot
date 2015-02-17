5afd3f58ba77971fb77b9642f896c5d1b64fdd5b
Erik Carlsson <info@code301.com>
2015-02-17 13:37:16 +0100

    Init

---
+++ lib/Badge/Depot.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Badge::Depot;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Badge::Depot;
+
+=head1 DESCRIPTION
+
+Badge::Depot is ...
+
+=head1 SEE ALSO
+
+=cut
